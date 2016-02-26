5d2ee4dce9ea44ab79dd670b949beaa84274c2fe
edvaldatli <eddi1122@hotmail.com>
2016-02-26 11:48:37 +0000

    Update Header.php

--- include/header.php
+++ include/header.php
@@ -1,7 +1,14 @@
 <?php
 
 ?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 <link rel="stylesheet" href="css/materialize.css">
 <link rel="stylesheet" href="css/style.css">
-<script type="text/javascript" src="../js/materialize.js"></script>
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
