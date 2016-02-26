9b565d2a907e05bb37777a31111f3c7779539f18
edvaldatli <eddi1122@hotmail.com>
2016-02-26 11:47:00 +0000

    Update Index.php

--- index.php
+++ index.php
@@ -15,9 +15,21 @@
 ?>
 <div class="container">
     <div class="row">
-        <div class="valign-wrapper">
-            <h5 class="valign">This should be vertically aligned</h5>
+        <div class="col s12 container-low valign-wrapper">
+            <h2 class="col offset-s1 offset-m1 offset-l2 white-text" >This should be vertically aligned</h2>
         </div>
+    </div>
+    <div class="row" id="3columns">
+        <div class="col s4">
+            <i class="large material-icons">language</i>
+        </div>
+        <div class="col s4">
+            <i class="large material-icons">thumbs_up_down</i>
+        </div>
+        <div class="col s4">
+            <i class="large material-icons">shop</i>
+        </div>
+
     </div>
 </div>
 
