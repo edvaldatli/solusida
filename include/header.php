c963175f81ee6a74a4c723ca2079392c215389d7
Aron Hrafnsson <aronhr98@gmail.com>
2016-02-29 10:47:39 +0000

    Title fyrir sidurnar

--- include/header.php
+++ include/header.php
@@ -1,13 +1,8 @@
-<?php
-
-?>
-<head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
+<title><?php echo "{$title}"; ?></title>
 <link rel="stylesheet" href="css/materialize.css">
 <link rel="stylesheet" href="css/style.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/materialize.js"></script>
---
+++ include/title.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0712982139
+ * Date: 29.2.2016
+ * Time: 10:43
+ */
+$title = basename($_SERVER['SCRIPT_FILENAME'], '.php');
+$title = ucfirst($title);
