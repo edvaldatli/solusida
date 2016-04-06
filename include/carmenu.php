597befa0d0e79a4eac3a02b755aa65fbe906fb0b
edvaldatli <eddi1122@hotmail.com>
2016-04-06 09:30:00 +0000

    Update

---
+++ include/carmenu.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<nav>
+    <div class="car-nav container nav-wrapper">
+        <div class="row">
+            <ul class="">
+                <li class="col s2 center"><a href="#">Golf Mk6</a></li>
+                <li class="col s2 center"><a href="#">Passat CC</a></li>
+                <li class="col s2 center"><a href="#">Polo Mk5</a></li>
+                <li class="col s2 center"><a href="#">Toureg II</a></li>
+                <li class="col s2 center"><a href="#">New Beetle</a></li>
+                <li class="col s2 center"><a href="#">Vento</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
