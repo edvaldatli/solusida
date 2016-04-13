8b14044a93fb640a801be79ce4cc50684bebd07d
edvaldatli <eddi1122@hotmail.com>
2016-04-13 13:54:42 +0000

    Update

--- item.php
+++ item.php
@@ -8,7 +8,7 @@ $data = new ADMIN();
 <head>
     <?php require "include/header.php"; ?>
 </head>
-<body>
+<body id="top">
 <?php include "include/menu.php"; ?>
 <div class="parallax-container">
     <div class="parallax"><img src="http://www.inopowers.net/images/2015/09/2016-Volkswagen-Tiguan-Crossover-Wallpaper.jpg"></div>
@@ -26,67 +26,19 @@ $data = new ADMIN();
     </div>
     <div id="model" class="col s12">
         <?php $data->load_cars();?>
-        $login->redirect('home.php');
-        <!-- ******************************************* Model *******************************************
-        <div class="divider"></div>
-        <div class="section">
-            <h5>Jetta</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>Passat</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>Beetle</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>Golf</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>Golf R</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>e-Golf</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>Tiguan</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>Tourareg</h5>
-            <p>Stuff</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <h5>SportWagen</h5>
-            <p>Stuff</p>
-        </div>-->
-
     </div>
+
     <div id="color" class="col s12">
-        <!-- ******************************************* Color *******************************************r -->
 
     </div>
     <div id="mod" class="col s12">
-        <!-- ******************************************* Mods ******************************************* -->
-
+        <span>Selecting a model first might be a smart move!</span>
     </div>
     <div id="pay" class="col s12">
-        <!-- ******************************************* Payment ******************************************* -->
-
+        <span>Selecting a model first might be a smart move!</span>
     </div>
 </div>
+
+<script type="text/javascript" src="js/payment.js"></script>
 </body>
 </html>
